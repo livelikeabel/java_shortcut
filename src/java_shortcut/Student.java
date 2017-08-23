1ba152c996c815b2744bf12b5e628bdcbcc0976a
abel <esung1129@gmail.com>
2017-08-23 20:20:47 +0900

    class 1

---
+++ src/java_shortcut/Student.java
@@ -0,0 +1,14 @@
+package java_shortcut;
+
+public class Student {
+	private String name;
+	
+	Student(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		
+		return this.name;
+	}
+}
---
+++ src/java_shortcut/StudentTest.java
@@ -0,0 +1,19 @@
+package java_shortcut;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StudentTest {
+	@Test
+	public void testCreate() {
+		final String firstStudentName = "Jane Doe";
+		Student firstStudent = new Student(firstStudentName);
+		assertEquals(firstStudentName, firstStudent.getName());
+		
+		final String secondStudentName = "Joe Blow";
+		Student secondStudent = new Student(secondStudentName);
+		assertEquals(secondStudentName, secondStudent.getName());
+	}
+
+}

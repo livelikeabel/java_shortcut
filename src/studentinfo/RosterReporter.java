58585f9ebd263a354da10cf68deca521056093ac
abel <esung1129@gmail.com>
2017-08-26 00:47:44 +0900

    class 3 / until 10
    8.sysout
    9.sysout
    10.refactoring

--- src/studentinfo/RosterReporter.java
+++ src/studentinfo/RosterReporter.java
@@ -17,18 +17,26 @@ public class RosterReporter {
 	
 	String getReport() {
 		StringBuilder buffer = new StringBuilder();
+		writeHeader(buffer);
+		writeBody(buffer);
+		writeFooter(buffer);
 		
+		return buffer.toString();
+	}
+	
+	void writeHeader(StringBuilder buffer) {
 		buffer.append(ROSTER_REPORT_HEADER);
-		
-		for (Student student : session.getAllStudents()) {
+	}
+	
+	void writeBody(StringBuilder buffer) {
+		for (Student student: session.getAllStudents()) {
 			buffer.append(student.getName());
 			buffer.append(NEWLINE);
 		}
-		
+	}
+	
+	void writeFooter(StringBuilder buffer) {
 		buffer.append(
-			ROSTER_REPORT_FOOTER + session.getAllStudents().size() + 
-			NEWLINE);
-		
-		return buffer.toString();
+			ROSTER_REPORT_FOOTER + session.getAllStudents().size() + NEWLINE);
 	}
 }
